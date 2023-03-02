31f49dd9ef63798219aec5a7af880ff2c37a6ef0
Chris Immel <chris.immel@newsela.com>
2023-03-02 14:06:00 +0100

    Activate sleep mode.

--- calliope/routes/v1/story.py
+++ calliope/routes/v1/story.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, Depends, Request
 from fastapi.security.api_key import APIKey
 from pydantic import BaseModel
 
-from calliope.models import FramesRequestParamsModel, StoryFrameModel, StoryModel
+from calliope.models import FramesRequestParamsModel, StoryFrameModel
 from calliope.storage.config_manager import get_sparrow_story_parameters_and_keys
 from calliope.storage.state_manager import (
     get_sparrow_state,
@@ -16,7 +16,7 @@ from calliope.storage.state_manager import (
     put_story,
 )
 from calliope.strategies import StoryStrategyRegistry
-from calliope.tables import Story, StoryFrame
+from calliope.tables import Image, Story, StoryFrame
 from calliope.utils.fastapi import get_base_url
 from calliope.utils.file import (
     create_sequential_filename,
@@ -66,7 +66,8 @@ async def post_frames(
     """
     base_url = get_base_url(request)
 
-    return await handle_frames_request(request_params, base_url)
+    # return await handle_frames_request(request_params, base_url)
+    return await handle_frames_request_sleep(request_params, base_url)
 
 
 @router.get("/frames/", response_model=StoryResponseV1)
@@ -81,7 +82,42 @@ async def get_frames(
     """
     base_url = get_base_url(request)
 
-    return await handle_frames_request(request_params, base_url)
+    # return await handle_frames_request(request_params, base_url)
+    return await handle_frames_request_sleep(request_params, base_url)
+
+
+async def handle_frames_request_sleep(
+    request_params: FramesRequestParamsModel,
+    base_url: str,
+) -> StoryResponseV1:
+
+    image = Image(
+        format="image/png", width=512, height=512, url="media/Calliope-sleeps.png"
+    )
+
+    frames = [
+        StoryFrame(
+            image=image,
+            source_image=image,
+            text="Calliope sleeps a dream of angels. She will awake shortly, stronger for having slept.",
+            metadata={},
+            min_duration_seconds=60,
+        )
+    ]
+
+    await prepare_frame_images(request_params, frames, save=False)
+
+    frame_models = [frame.to_pydantic() for frame in frames]
+
+    response = StoryResponseV1(
+        frames=frame_models,
+        append_to_prior_frames=False,
+        request_id=cuid.cuid(),
+        generation_date=str(datetime.utcnow()),
+        debug_data={},
+        errors=[],
+    )
+    return response
 
 
 async def handle_frames_request(
@@ -142,7 +178,7 @@ async def handle_frames_request(
         "thoth_link": f"{base_url}thoth/story/{story.cuid}",
     }
 
-    await prepare_frame_images(parameters, story_frames_response.frames, base_url)
+    await prepare_frame_images(parameters, story_frames_response.frames)
 
     await put_story(story)
     await put_sparrow_state(sparrow_state)
@@ -189,7 +225,9 @@ async def prepare_input_files(
 
 
 async def prepare_frame_images(
-    parameters: FramesRequestParamsModel, frames: List[StoryFrame], base_url: str
+    parameters: FramesRequestParamsModel,
+    frames: List[StoryFrame],
+    save: bool = True,
 ) -> None:
     is_google_cloud = is_google_cloud_run_environment()
     client_id = parameters.client_id
@@ -199,34 +237,46 @@ async def prepare_frame_images(
         # image = await frame.get_related(StoryFrame.image)
         image = frame.image
         if image:
+            image_updated = False
+
             if image_is_monochrome(image.url):
                 print(f"Image {image.url} is monochrome. Skipping.")
                 # Skip the image if it has only a single color (usually black).
                 # (This doesn't appear to work.)
                 frame.image = None
-                await frame.save().run()
+                if save:
+                    await frame.save().run()
                 continue
 
             output_image_width = parameters.output_image_width
             output_image_height = parameters.output_image_height
+            if save and is_google_cloud:
+                # Save the original PNG image in case we want to see it later.
+                put_media_file(image.url)
+            original_image = image
             image = resize_image_if_needed(
                 image, output_image_width, output_image_height
             )
+            image_updated = image.id != original_image.id
 
             if output_image_format == ImageFormat.RGB565:
-                if is_google_cloud:
-                    # Also save the original PNG image in case we want to see it later.
-                    put_media_file(image.url)
                 base_filename = get_base_filename(image.url)
                 output_image_filename_raw = f"media/{base_filename}.raw"
                 image = convert_png_to_rgb565(image.url, output_image_filename_raw)
+                frame.image = image
+                image_updated = True
             elif output_image_format == ImageFormat.GRAYSCALE16:
-                if is_google_cloud:
+                if save and is_google_cloud:
                     # Also save the original PNG image in case we want to see it later.
                     put_media_file(image.url)
                 base_filename = get_base_filename(image.url)
                 output_image_filename_raw = f"media/{base_filename}.grayscale16"
                 image = convert_png_to_grayscale16(image.url, output_image_filename_raw)
+                frame.image = image
 
-            if is_google_cloud:
-                put_media_file(image.url)
+            if image_updated and save:
+                frame.image = image
+                await image.save().run()
+                await frame.save().run()
+                if is_google_cloud:
+                    put_media_file(image.url)
