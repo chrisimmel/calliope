8e61cb6278aa880a452c121354e13d287d0cb81a
chrisimmel <chris.immel@gmail.com>
2023-07-14 05:23:41 +0200

    Move inference logic to own module.

---
+++ calliope/inference/__init__.py
@@ -0,0 +1,13 @@
+from .inference import (
+    caption_to_prompt,
+    image_analysis_inference,
+    text_to_extended_text_inference,
+    text_to_image_file_inference,
+)
+
+__all__ = [
+    "caption_to_prompt",
+    "image_analysis_inference",
+    "text_to_extended_text_inference",
+    "text_to_image_file_inference",
+]
