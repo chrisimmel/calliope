20c44746822dce77abfa015eb75416c1d840ef04
chrisimmel <chris.immel@gmail.com>
2024-07-01 18:08:21 +0200

    Tamarisk-adjacent things,

--- calliope/strategies/__init__.py
+++ calliope/strategies/__init__.py
@@ -1,8 +1,10 @@
 from .base import StoryStrategy
 from .continuous_v0 import ContinuousStoryV0Strategy
 from .continuous_v1 import ContinuousStoryV1Strategy
+from .tamarisk import TamariskStrategy
 from .lavender import LavenderStrategy
 from .literal import LiteralStrategy
+
 # from .show_this_frame import ShowThisFrameStrategy
 from .simple_one_frame import SimpleOneFrameStoryStrategy
 from .registry import StoryStrategyRegistry
@@ -10,6 +12,7 @@ from .registry import StoryStrategyRegistry
 __all__ = [
     "ContinuousStoryV0Strategy",
     "ContinuousStoryV1Strategy",
+    "TamariskStrategy",
     "LavenderStrategy",
     "LiteralStrategy",
     # "ShowThisFrameStrategy",
