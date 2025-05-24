dcf70468a9d17c103144e08177289a1fc5c0e1f3
chrisimmel <chris.immel@gmail.com>
2025-05-24 13:15:38 +0200

    Remove unused route.

--- calliope/app.py
+++ calliope/app.py
@@ -24,7 +24,6 @@ from calliope.routes.v1 import story as story_routes
 from calliope.routes.v1 import config as config_routes
 from calliope.routes.v1 import test as test_routes
 from calliope.routes.v1 import bookmark as bookmark_routes
-from calliope.routes.v1 import connectivity as connectivity_routes
 from calliope.utils.authentication import get_api_key
 from calliope.utils.google import is_google_cloud_run_environment
 from calliope.settings import settings
@@ -56,7 +55,6 @@ def register_views(app: FastAPI) -> None:
     app.include_router(thoth_routes.router)
     app.include_router(test_routes.router)
     app.include_router(bookmark_routes.router)
-    app.include_router(connectivity_routes.router)
 
 
 def get_db_uri(user: str, passwd: str, host: str, db: str) -> str:
